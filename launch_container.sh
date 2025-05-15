baf06494b78bb885135953c1407b715fca8e18cc
JaimeG-ELC <zocoblo1@gmail.com>
2025-05-15 18:43:32 +0200

    fix: remove unnecessary blank lines in launch_container.sh

--- launch_container.sh
+++ launch_container.sh
@@ -3,7 +3,6 @@ docker run -it --privileged --net=host \
     -v /dev:/dev \
     -v /etc/udev/rules.d:/etc/udev/rules.d \
     --device /dev/sensors/vesc:/dev/sensors/vesc \
-
     --env DISPLAY=$DISPLAY \
     --volume /tmp/.X11-unix:/tmp/.X11-unix \
     --volume ~/.Xauthority:/root/.Xauthority \
@@ -11,5 +10,4 @@ docker run -it --privileged --net=host \
     --volume=./src/particle_filter_pkg:/root/coche_ws/src/particle_filter_pkg \
     --volume=./src/gap_follow_pkg:/root/coche_ws/src/gap_follow_pkg \
     --volume=./src/pure_persuit_pkg:/root/coche_ws/src/pure_persuit_pkg \
-
     f1tenth-system
