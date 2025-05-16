3c82cf7694b0a92517e3ff29ddf6418aadaf2429
JaimeG-ELC <zocoblo1@gmail.com>
2025-05-16 18:17:59 +0200

    refactor: migrate mpc package to new structure and remove old implementation

--- launch_container.sh
+++ launch_container.sh
@@ -10,6 +10,6 @@ docker run -it --privileged --net=host \
     --volume=./src/particle_filter_pkg:/root/coche_ws/src/particle_filter_pkg \
     --volume=./src/gap_follow_pkg:/root/coche_ws/src/gap_follow_pkg \
     --volume=./src/pure_persuit_pkg:/root/coche_ws/src/pure_persuit_pkg \
-    --volume=./src/pure_persuit_pkg:/root/coche_ws/src/pure_persuit_pkg \
+    --volume=./src/mpc_pkg:/root/coche_ws/src/mpc_pkg \
 
     f1tenth-system
