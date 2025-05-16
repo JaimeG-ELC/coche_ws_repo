d6240c76221572729280bd91a348e09d83c0fe3c
JaimeG-ELC <zocoblo1@gmail.com>
2025-05-16 17:45:16 +0200

    fix: update volume path for f1tenth_stack in launch_container.sh

--- launch_container.sh
+++ launch_container.sh
@@ -6,7 +6,7 @@ docker run -it --privileged --net=host \
     --env DISPLAY=$DISPLAY \
     --volume /tmp/.X11-unix:/tmp/.X11-unix \
     --volume ~/.Xauthority:/root/.Xauthority \
-    --volume=./src/f1tenth_stack_pkg:/root/coche_ws/src/f1tenth_stack_pkg  \
+    --volume=./src/f1tenth_stack:/root/coche_ws/src/f1tenth_stack  \
     --volume=./src/particle_filter_pkg:/root/coche_ws/src/particle_filter_pkg \
     --volume=./src/gap_follow_pkg:/root/coche_ws/src/gap_follow_pkg \
     --volume=./src/pure_persuit_pkg:/root/coche_ws/src/pure_persuit_pkg \
