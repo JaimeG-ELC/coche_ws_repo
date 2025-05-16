70ce8b3f442dba8afac8fc887880343a2bb32542
JaimeG-ELC <zocoblo1@gmail.com>
2025-05-16 18:32:19 +0200

    refactor: update volume mapping in launch_container.sh to replace particle_filter_pkg with trayectoria_pkg

--- launch_container.sh
+++ launch_container.sh
@@ -7,9 +7,9 @@ docker run -it --privileged --net=host \
     --volume /tmp/.X11-unix:/tmp/.X11-unix \
     --volume ~/.Xauthority:/root/.Xauthority \
     --volume=./src/f1tenth_stack:/root/coche_ws/src/f1tenth_stack  \
-    --volume=./src/particle_filter_pkg:/root/coche_ws/src/particle_filter_pkg \
+    --volume=./src/trayectoria_pkg:/root/coche_ws/src/trayectoria_pkg \
     --volume=./src/gap_follow_pkg:/root/coche_ws/src/gap_follow_pkg \
     --volume=./src/pure_persuit_pkg:/root/coche_ws/src/pure_persuit_pkg \
     --volume=./src/mpc_pkg:/root/coche_ws/src/mpc_pkg \
-
+    --volume=./src/mpc_pkg:/root/coche_ws/src/mpc_pkg \
     f1tenth-system
