fc5502fed528501732ffe6a05969d26851f51bdd
JaimeG-ELC <zocoblo1@gmail.com>
2025-05-20 00:16:28 +0200

    feat: add waypoint generator package volume to launch container script

--- launch_container.sh
+++ launch_container.sh
@@ -11,4 +11,5 @@ docker run -it --privileged --net=host \
     --volume=./src/gap_follow_pkg:/root/coche_ws/src/gap_follow_pkg \
     --volume=./src/pure_pursuit_pkg:/root/coche_ws/src/pure_pursuit_pkg \
     --volume=./src/mpc_pkg:/root/coche_ws/src/mpc_pkg \
+    --volume=./src/waypoint_generator_pkg:/root/coche_ws/src/waypoint_generator_pkg \
     f1tenth-system
