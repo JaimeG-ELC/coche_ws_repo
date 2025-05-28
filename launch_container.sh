17a52766a66292ffb842ae55cfc823656ff42fcf
JaimeG-ELC <zocoblo1@gmail.com>
2025-05-28 17:25:37 +0200

    feat: add manual control package volume to launch container script

--- launch_container.sh
+++ launch_container.sh
@@ -12,4 +12,5 @@ docker run -it --privileged --net=host \
     --volume=./src/pure_pursuit_pkg:/root/coche_ws/src/pure_pursuit_pkg \
     --volume=./src/mpc_pkg:/root/coche_ws/src/mpc_pkg \
     --volume=./src/waypoint_generator_pkg:/root/coche_ws/src/waypoint_generator_pkg \
+    --volume=./src/manual_control_pkg:/root/coche_ws/src/manual_control_pkg \
     f1tenth-system
