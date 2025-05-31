40eac20ae690cca59409928adf5f43fa3344df58
JaimeG-ELC <zocoblo1@gmail.com>
2025-05-31 19:15:23 +0200

    fix: update launch files to include manual control and sensor configurations

--- src/f1tenth_stack/launch/bringup.launch.py
+++ src/f1tenth_stack/launch/bringup.launch.py
@@ -101,11 +101,17 @@ def generate_launch_description():
         name='urg_node',
         parameters=[LaunchConfiguration('sensors_config')]
     )
-    static_tf_node = Node(
+    static_tf_baselink_basefootprint_node = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
-        name='static_baselink_to_laser',
-        arguments=['0.27', '0.0', '0.11', '0.0', '0.0', '0.0', 'base_link', 'laser']
+        name='static_baselink_to_basefootprint',
+        arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'base_link', 'base_footprint']
+    )
+    static_tf_basefootprint_laser_node = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='static_basefootprint_to_laser',
+        arguments=['0.27', '0.0', '0.11', '0.0', '0.0', '0.0', 'base_footprint', 'laser']
     )
 
     # finalize
@@ -115,6 +121,7 @@ def generate_launch_description():
     ld.add_action(vesc_to_odom_node)
     ld.add_action(vesc_driver_node)
     ld.add_action(urg_node)
-    ld.add_action(static_tf_node)
+    ld.add_action(static_tf_footprint_basefootprint_node)
+    ld.add_action(static_tf_basefootprint_laser_node)
 
     return ld
--- src/f1tenth_stack/launch/mapping.launch.py
+++ src/f1tenth_stack/launch/mapping.launch.py
@@ -37,15 +37,87 @@ def generate_launch_description():
         'config',
         'f1tenth_online_async.yaml'
     )
-    # Declare launch arguments
+    vesc_config = os.path.join(
+        get_package_share_directory('f1tenth_stack'),
+        'config',
+        'vesc.yaml'
+    )
+    sensors_config = os.path.join(
+        get_package_share_directory('f1tenth_stack'),
+        'config',
+        'sensors.yaml'
+    )
+    manual_control_config = os.path.join(
+        get_package_share_directory('f1tenth_stack'),
+        'config',
+        'manual_control.yaml'
+    )
+    # Launch arguments
+
+    manual_control_la = DeclareLaunchArgument(
+        'manual_control_config',
+        default_value=manual_control_config,
+        description='Descriptions for manual_control configs'
+    )
+    vesc_la = DeclareLaunchArgument(
+        'vesc_config',
+        default_value=vesc_config,
+        description='Descriptions for vesc configs')
+    sensors_la = DeclareLaunchArgument(
+        'sensors_config',
+        default_value=sensors_config,
+        description='Descriptions for sensor configs')
     mapping_la = DeclareLaunchArgument(
         'mapping_config',
          default_value=mapping_config,
          description='Descriptions for mapping configs'
     )
 
-    ld = LaunchDescription([mapping_la])
+    ld = LaunchDescription([mapping_la, manual_control_la, vesc_la, sensors_la])
 
+        joy_node = Node(
+        package='joy',
+        executable='joy_node',
+        name='joy_node',
+        output='screen'
+    )
+    manual_control_node = Node(
+        package='manual_control_pkg',
+        executable='manual_control_node',
+        name='manual_control_node',
+        output='screen',
+        parameters=[LaunchConfiguration('manual_control_config')]
+    )
+    ackermann_to_vesc_node = Node(
+        package='vesc_ackermann',
+        executable='ackermann_to_vesc_node',
+        name='ackermann_to_vesc_node',
+        parameters=[LaunchConfiguration('vesc_config')]
+    )
+    vesc_to_odom_node = Node(
+        package='vesc_ackermann',
+        executable='vesc_to_odom_node',
+        name='vesc_to_odom_node',
+        parameters=[LaunchConfiguration('vesc_config')]
+    )
+    vesc_driver_node = Node(
+        package='vesc_driver',
+        executable='vesc_driver_node',
+        name='vesc_driver_node',
+        parameters=[LaunchConfiguration('vesc_config')]
+    )
+    urg_node = Node(
+        package='urg_node',
+        executable='urg_node_driver',
+        name='urg_node',
+        parameters=[LaunchConfiguration('sensors_config')]
+    )
+    static_tf_node = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='static_baselink_to_laser',
+        arguments=['0.27', '0.0', '0.11', '0.0', '0.0', '0.0', 'base_link', 'laser']
+    )
     mapping_node = Node(
         package='slam_toolbox',
         executable='async_slam_toolbox_node',
